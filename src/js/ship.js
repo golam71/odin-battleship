0d64700f55284ff6e35c6f317bf823b9bb0150b7
golam71 <74001687+golam71@users.noreply.github.com>
2025-08-05 15:15:46 +0600

    :ambulance: critical fix on tests

--- src/js/ship.js
+++ src/js/ship.js
@@ -6,8 +6,18 @@ export default class Ship {
     this.hits = 0;
   }
 
-  hit() {
-    this.hits++;
+  hit(x, y) {
+    if (
+      this.location.some(
+        (position) => position.toString() === [x, y].toString()
+      )
+    ) {
+      this.hits++;
+    }
+  }
+
+  health() {
+    return this.length - this.hits;
   }
 
   isSunk() {
--- test/ship.test.js
+++ test/ship.test.js
@@ -1,7 +1,25 @@
 import Ship from "../src/js/ship.js";
 
-const ship = new Ship("Destroyer", 2, [0, 1]);
+const ship = new Ship("Destroyer", 2, [
+  [0, 1],
+  [1, 2],
+]);
 
 test("Ship to have name", () => {
-  expect(ship.name === "Destroyer");
+  expect(ship.name).toBe("Destroyer");
+});
+
+test("Ship to have health", () => {
+  expect(ship.health()).not.toBe(0);
+});
+
+test("Ship to take hits", () => {
+  ship.hit(1, 2);
+  expect(ship.health()).toBe(1);
+});
+
+test("Ship to Sink", () => {
+  expect(ship.isSunk()).toBe(false);
+  ship.hit(0, 1);
+  expect(ship.isSunk()).toBe(true);
 });
